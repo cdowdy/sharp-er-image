c5b60fcef1bd2117af9267a3b1beb2a340b6e26f
Cory Dowdy <cdowdy46@gmail.com>
2019-12-18 12:07:54 -0500

    changes validImageExtension to validOutput

--- tests/ValidOutputImageExtension.js
+++ tests/ValidOutputImageExtension.js
@@ -1,5 +1,5 @@
 import test from "ava";
-import validImageExtension from '../lib/validOutputExtensions';
+import validOutput from '../lib/validOutputExtensions';
 import path from 'path';
 const imagePath = path.join(__dirname, '../tests/images');
 
@@ -7,21 +7,21 @@ const imagePath = path.join(__dirname, '../tests/images');
 
 test( 'Proper Image Format', async t => {
     t.true(
-        await validImageExtension( path.parse(imagePath + 'olga-kononenko-FdSD_9r8Uy8.jpeg').ext )
+        await validOutput( path.parse(imagePath + 'olga-kononenko-FdSD_9r8Uy8.jpeg').ext )
     );
     t.true(
-        await validImageExtension( path.parse(imagePath + 'olga-kononenko-FdSD_9r8Uy8.jpg').ext )
+        await validOutput( path.parse(imagePath + 'olga-kononenko-FdSD_9r8Uy8.jpg').ext )
     );
     t.true(
-        await validImageExtension( path.parse(imagePath + 'olga-kononenko-FdSD_9r8Uy8.webp').ext )
+        await validOutput( path.parse(imagePath + 'olga-kononenko-FdSD_9r8Uy8.webp').ext )
     );
     t.true(
-        await validImageExtension( path.parse(imagePath + 'olga-kononenko-FdSD_9r8Uy8.png').ext )
+        await validOutput( path.parse(imagePath + 'olga-kononenko-FdSD_9r8Uy8.png').ext )
     );
 });
 
 test( "Unsupported Image Format", async t => {
     t.false(
-        await validImageExtension( path.parse(imagePath + 'olga-kononenko-FdSD_9r8Uy8.gif').ext )
+        await validOutput( path.parse(imagePath + 'olga-kononenko-FdSD_9r8Uy8.gif').ext )
     );
 });
